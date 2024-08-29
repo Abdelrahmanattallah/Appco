23ddce9f226d6d30b10f82bf9bb80931b1346953
Abdelrahman Attallah <abdelrahmanattalla45@gmail.com>
2024-08-29 21:19:07 +0300

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// variables
+var first = {
+  name: "shaker", //string
+  age: 25, //number
+  skills: ["html", "css", "javascript"], //array
+  method: function () {
+    //functions
+    console.log(this);
+  },
+  avail: false, //boolean
+  disc: undefined, //undefined
+  inEgypt: true, //boolean
+};
+let second = "second";
+const third = "third";
+
+// data types
